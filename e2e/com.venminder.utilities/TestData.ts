619482963129fdc6e9a0205f1568561195458e7e
Pawan Singh <singh.pawan659@gmail.com>
2018-09-26 14:59:29 +0530

    Adding code in repo

---
+++ e2e/com.venminder.utilities/TestData.ts
@@ -0,0 +1,21 @@
+export class siteUrls {
+  sand_dev_1_url = "https://sand-dev-1-rsd.venminder.com";
+  sand_dev_2_url = "https://sand-dev-2-rsd.venminder.com";
+  sand_dev_3_url = "https://sand-dev-3-rsd.venminder.com";
+  sand_gets_1_url = "https://sand-gets-1-app.venminder.com";
+}
+export class userData {
+  admin_username = "testy.tester@entclient1.com";
+  dc_admin_username = "venminder.admin@venminder.com";
+  password = "Digital123";
+}
+
+export class database {
+  port = "1433";
+  venminder_db_service = "sandbox-venminder.database.windows.net";
+  venminder_vcms_db = "SAND-DEV-1-VCMS";
+  venminder_email_db = "SAND-DEV-1-Email";
+  venminder_logging_db = "SAND-DEV-1-Logging";
+  venminder_db_user = "3Pillar";
+  venminder_db_password = "p37*4W7VG78s";
+}
